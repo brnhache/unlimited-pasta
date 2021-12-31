f2212e5c38eec76069f10405660532a975ccc128
brian <brnhache@gmail.com>
2021-12-31 18:06:46 -0500

    [UP-0006] Fixes the editing bug so that it does not update until the OK button is pressed. Updates unneccessarily when the dialog is opened, however. Must be a better way.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 
 
@@ -33,6 +34,7 @@ import { FormsModule } from '@angular/forms';
     MatButtonModule,
     MatDialogModule,
     MatFormFieldModule,
+    MatInputModule,
     FormsModule,
     AngularFireModule.initializeApp(env.firebase),
     AngularFirestoreModule,
